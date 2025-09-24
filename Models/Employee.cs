a876d1c232d2503f19fdcdc986cc87944917d4e7
vannhi1604 <vannhi16042005@gmail.com>
2025-09-24 15:31:29 +0700

    thêm class employee

---
+++ Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace ltweb_bt5.Models
+{
+    public class Employee
+    {
+        public long id { get; set; }
+        public string fullName { get; set; }
+        public string gender { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public double salary { get; set; }
+        public string status { get; set; }
+
+    }
+}
