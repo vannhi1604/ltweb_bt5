4b9a38b492fa4a33527462a7a71f963e0a5bad0d
vannhi1604 <vannhi16042005@gmail.com>
2025-10-02 20:22:22 +0700

    Tạo usercontroller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using ltweb_bt5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ltweb_bt5.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult UserManualValid()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UserManualValid(User user)
+        {
+            String pass = user.Password;
+            if (pass.Length < 7)
+            {
+                ViewBag.PassErr = "Mat khau phai co toi thieu 7 ki tu";
+                return View();
+            }
+            return Content("Ban da nhap dung");
+        }
+        public IActionResult UserAnnotation()
+        {
+            return View();
+        }
+        public IActionResult UserAnnotation(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content("user state valid");
+            }
+
+            return View();
+        }
+    }
+}
